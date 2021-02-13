9604071a58e4670e9374bbbc06bdfe3c6f06a5a1
David Brown <david@djb.org.uk>
2021-02-13 14:15:32 +0000

    Removed retain from event messages

--- src/server.ts
+++ src/server.ts
@@ -13,11 +13,11 @@ const siaServer = new SIAServer(config.sia)
 
 // This is used to publish updates to zone entities
 // it publishes to $baseTopic/zone
-siaServer.on('ZoneEvent', async function (event: Event) {
-    const zoneConfig = config.zones[event.zone]
-    const handler = getZoneEventHandler(zoneConfig)
-    await handler.handleZoneEvent(event, publisher)
-})
+// siaServer.on('ZoneEvent', async function (event: Event) {
+//     const zoneConfig = config.zones[event.zone]
+//     const handler = getZoneEventHandler(zoneConfig)
+//     await handler.handleZoneEvent(event, publisher)
+// })
 
 // This is used when there is a system event such as set/unset
 // it publishes to $baseTopic/set|alarm|comms
@@ -28,5 +28,5 @@ siaServer.on('SystemEvent', async function (event: Event) {
 // This is used to publish raw event data
 // it publishes to $baseTopic/event
 siaServer.on('Event', async function(event: Event) {
-    await publisher.publishJSON("event", event, true)
+    await publisher.publishJSON("event", event, false)
 })
