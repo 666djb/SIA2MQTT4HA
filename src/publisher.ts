4f045ddbc686a89148eb8b6687e36dd7dfd1eeb3
David Brown <david@djb.org.uk>
2022-01-02 14:58:43 +0000

    Updated to publish set and armed states

--- src/publisher.ts
+++ src/publisher.ts
@@ -188,8 +188,8 @@ export class Publisher {
             await this.publishJSON("comms_test", {status: "None yet", time: "00:00"}, true)
 
             // Set initial statuses for binary entities
-            await this.publishJSON("armed", {state: false}, true)
-            await this.publishJSON("alarm", {state: false}, true)
+            await this.publishJSON("armed", {state: false})
+            await this.publishJSON("alarm", {state: false})
 
         } catch (ex) {
             console.log(ex)
